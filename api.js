8767d6d47f665bfc4ee7430f76ca4d502cea9192
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-03-07 21:20:12 +0300

    Upload Api File 1 And Requements For Api

---
+++ api.js
@@ -0,0 +1,50 @@
+// Lesson 1
+// API << Application Programming Interface >>
+// يستخدم في كل المجالات البرمجية وذلك لاستدعاء البيانات من الباك ايند
+
+// REST API << Representational State Transfer >> || RESTFul API
+// نفس الاي بي اي ولكن تكون متصلة باحد قواعد البيانات
+// تستخدم لرفع تعديلات أو جلب نتائج أو اي شيء يكون مقتصر في التعامل مع قواعد البيانات
+// الاستخدام الأكبر هو جلب البيانات مثل سعر الصرف الدولار أو الطقس أو اي شيء
+
+// دائماً نحصل على جيسون عند التعامل مع ال أي بي آي
+
+// Lesson 2
+// JSON << JavaScript Object Notation >>
+// يستخدم لتبادل البيانات بين المتصفح والخادم
+// يكون خفيف الوزن اثناء عملية التبادل لذلك يعد أفضل خيار للتبادل
+/**
+ * json is a text format for storing and transporting data
+ * تصف نفسها بنفسها وسهلة الفهم بإذن الله
+ * 1 - json is ===> {}
+ * 2 - json is a key:value
+ * 3 - key is a string ""
+ * 4 - نفصل بين المفاتيح بفاصلة
+ * 5 - [] for arrays
+ * 6 - {} for objects
+ * 7 - Right Data is ===> String, Boolean, Number, Object, Array, Null
+ *
+ * Example :
+ *
+ * let json = {
+ * "String" : "Mohamed Nour",
+ * "Number" : 100,
+ * "Object" : {
+ * "Sy" : "Syria",
+ * "Gr" : "Gordon",
+ * },
+ * "Array" : ["Ahmad", "Nour", "Ali"],
+ * "Boolean" : true,
+ * "Null" : null
+ * };
+ */
+
+// Lesson 3
+// JSON Methods :
+// 1 - JSON.parse(jsonName);
+// يحول ال جيسون من وضع السلسلة إلى وضعية الاوبجكت العادي
+// 2 - JSON.stringify(jsonName);
+// يحول الاوبجكت الى سلسلة من نوع جيسون
+
+// Lesson 4
+// Explanation Of API And Examples About It
