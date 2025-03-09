eaa4eadaef70a7fb36bebb39655e3c21805cde9e
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-03-09 21:02:35 +0300

    Upload How can you Get API For Playlist On Youtube

--- fetch_data.js
+++ fetch_data.js
@@ -27,3 +27,24 @@ fetch("https://api.github.com/users/nourmohamd/repos")
   .catch((reason) => {
     console.log(reason);
   });
+
+// How can you get api from playlist on youtube channal:
+// 1 - Go to https://developers.google.com/youtube/v3/docs/playlistItems/list#usage
+// 2 - Make URL Same as down:
+/*
+https://www.googleapis.com/youtube/v3/playlistItems
+?
+part=snippet
+&
+playlistId=رابط السلسلة من اليوتيوب
+&
+maxResults=0-->50
+&
+key=key from << Console Developers Google >> ---> Build Project 
+And Enable Youtube Api Then Create Credentials And Copy The Key
+
+Example:
+https://www.googleapis.com/youtube/v3/playlistItems?
+part=snippet&playlistId=PLMTdZ61eBnyqzVhegrlKy38Zwzky-eugX&maxResults=29
+&key=AIzaSyCXZ_AzTTpxS2ZALEmy_h7uUPphibYb7pc#
+*/
